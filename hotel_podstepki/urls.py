f50d8005030cc123dca2189becfde18ee3b8c787
Евгений Волков <evgeniy.volkov@me.com>
2024-10-16 18:10:00 +0400

    Docker update

--- hotel_podstepki/urls.py
+++ hotel_podstepki/urls.py
@@ -1,13 +1,11 @@
 """
 URL configuration for hotel_podstepki project.
 """
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.auth import views as auth_views
-
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
